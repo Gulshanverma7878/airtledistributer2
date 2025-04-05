c3a6c47f16bb246d5aa26730b9361c4ed1af73b6
Gulshanverma7878 <gamerronak9@gmail.com>
2025-04-05 13:48:05 +0530

    add commint

--- app/bank/page.js
+++ app/bank/page.js
@@ -70,7 +70,7 @@ const BankPage = () => {
         onClick={() => setIsFormOpen(true)}
         className="bg-blue-600 text-white px-4 py-2 rounded mb-4"
       >
-        Add Bank ||
+        Add Bank || ADD
       </button>
 
       </div>
